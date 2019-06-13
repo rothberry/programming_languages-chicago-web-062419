649994afce21800276959d020d9fdc419881e6f7
Phil Roth <phil.roth077@gmail.com>
2019-06-13 20:56:27 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,9 +4,11 @@ def reformat_languages(languages)
   new_hash = {}
   #binding.pry
   languages.each do |style, description|
-    binding.pry
+    #binding.pry
     description.each do |lang, type|
       binding.pry
+      new_hash[lang] = type
+      new_hash[lang][:style] = style.to_a
     end
   end
 
